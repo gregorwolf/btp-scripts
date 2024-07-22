321f147c45e997c009483e4c4e1368f0b832ed22
Gregor Wolf <gregor.wolf@gmail.com>
2024-07-22 14:47:50 +0200

    Identify Subaccounts with SAML IdPs

---
+++ identify-saml-idps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Identify Subaccounts with SAML IdPs
+#
+btp --format json list accounts/subaccount > output/subaccounts.json
+# Get the subaccount id using jq and loop through the subaccounts
+
+cat output/subaccounts.json | jq -r '.value[] | .guid, .subdomain' | while IFS=, read -r subaccount; do
+  read subdomain
+  # echo "Subaccount: $subaccount, subdomain: $subdomain"
+  btp --format json list security/trust -sa $subaccount > output/trust.json
+  cat output/trust.json | jq -r '.[].originKey' | while read -r originKey; do
+    btp --format json get security/trust $originKey -sa $subaccount > output/trust-details.json
+    cat output/trust-details.json | jq -r '.protocol' | while read -r protocol; do
+      # When protocol is SAML, then print the Subaccount / Origin Details
+      if [ "$protocol" == "SAML" ]; then
+        echo "subdomain: $subdomain"
+        echo "originKey: $originKey"
+      fi
+    done
+  done
+done
